a7a945fe3f2a7d910bbb01714c9e43e4915455c5
Pranit Rai <pranitrai07@gmail.com>
2026-09-19 09:21:56 +0530

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string checker(int i, int j, string& s, int len){
+        int n = s.size();
+        string p = "";
+        while (i>=0 && j<n && s[i] == s[j]){
+            i--;
+            j++;
+        }
+
+        if ((j-i-1) <= len) return "";
+        
+        return s.substr(i+1, j-i-1);
+    }
+
+    string longestPalindrome(string s) {
+        int mx = 0, n = s.size();
+        string ans = "";
+
+        for (int ind=0; ind<n; ind++){
+            string p = checker(ind, ind+1, s, mx);
+            if (p.size() > mx){
+                mx = p.size();
+                ans = p;
+            }
+
+            p = checker(ind, ind, s, mx);
+            if (p.size() > mx){
+                mx = p.size();
+                ans = p;
+            }
+        }
+
+        return ans;
+    }
+};
